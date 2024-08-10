6feb54b2c32f8bbb1ef86fae6edc37fa7979245c
HanquanL <hanquanl0115@gmail.com>
2024-08-10 11:57:17 -0400

    first commit

---
+++ src/main/java/com/MiniMe/CoreFileManagement/CoreFileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.MiniMe.CoreFileManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFileManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreFileManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MiniMe/CoreFileManagement/CoreFileManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiniMe.CoreFileManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreFileManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
