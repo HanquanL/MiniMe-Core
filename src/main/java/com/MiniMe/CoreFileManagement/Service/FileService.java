7143d7c831bee48de232498e92d3c2ea982ea603
HanquanL <Hanquanl0115@gmail.com>
2024-08-16 17:42:03 -0400

    Add services

---
+++ src/main/java/com/MiniMe/CoreFileManagement/Service/FileService.java
@@ -0,0 +1,31 @@
+package com.MiniMe.CoreFileManagement.Service;
+
+import com.MiniMe.CoreFileManagement.Entity.File;
+import com.MiniMe.CoreFileManagement.Repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FileService {
+    @Autowired
+    private FileRepository fileRepository;
+
+    public List<File> getAllFiles() {
+        return fileRepository.findAll();
+    }
+
+    public Optional<File> getFileById(Long id) {
+        return fileRepository.findById(id);
+    }
+
+    public File saveFile(File file) {
+        return fileRepository.save(file);
+    }
+
+    public void deleteFile(Long id) {
+        fileRepository.deleteById(id);
+    }
+}
