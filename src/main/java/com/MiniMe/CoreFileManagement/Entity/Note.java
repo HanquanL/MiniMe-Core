d2d980d6ad9a4c3b29c9260e6bc685e0fc4a2b83
HanquanL <Hanquanl0115@gmail.com>
2024-08-16 13:57:41 -0400

    ADD note entiry and note repository

---
+++ src/main/java/com/MiniMe/CoreFileManagement/Entity/Note.java
@@ -0,0 +1,18 @@
+package com.MiniMe.CoreFileManagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private Date uploadDate;
+}
