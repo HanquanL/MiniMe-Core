41834403aa13de2ff615eb1aa61fb7fdeea8f187
HanquanL <Hanquanl0115@gmail.com>
2024-08-17 09:52:10 -0400

    Add controllers

---
+++ src/main/java/com/MiniMe/CoreFileManagement/Controller/UserController.java
@@ -0,0 +1,39 @@
+package com.MiniMe.CoreFileManagement.Controller;
+
+import com.MiniMe.CoreFileManagement.Entity.File;
+import com.MiniMe.CoreFileManagement.Service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private FileService fileService;
+
+    @GetMapping
+    public List<File> getAllFiles() {
+        return fileService.getAllFiles();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<File> getFileById(@PathVariable Long id) {
+        Optional<File> file = fileService.getFileById(id);
+        return file.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public File saveFile(@RequestBody File file) {
+        return fileService.saveFile(file);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFile(@PathVariable Long id) {
+        fileService.deleteFile(id);
+        return ResponseEntity.noContent().build();
+    }
+}
